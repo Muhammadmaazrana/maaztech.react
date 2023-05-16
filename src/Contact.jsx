806e91d2bddc3aeb8e0119869d4bd1ad7bc4b3cf
MUHAMMAD MAAZ RANA <80382672+Muhammadmaazrana@users.noreply.github.com>
2023-05-16 20:05:51 +0500

    fix error

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -14,7 +14,7 @@ export default function Contact() {
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="+9230828372"/>
 </div>
 <div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <label for="exampleFormControlTextarea1" class="form-label">Message</label>
   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 </div>
 <button type="submit" class="btn btn-primary">Submit</button>
