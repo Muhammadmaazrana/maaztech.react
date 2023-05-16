b00493d68013ebb34d472798c84e2428aa203b7c
MUHAMMAD MAAZ RANA <80382672+Muhammadmaazrana@users.noreply.github.com>
2023-05-16 13:59:43 +0500

    error check

--- src/Services.jsx
+++ src/Services.jsx
@@ -3,6 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import web from "../src/images/pic3.png"
 
 
 
@@ -16,7 +17,7 @@ export default function Services() {
           <Card sx={{ maxWidth: 300 }}>
             <CardMedia
               sx={{ height: 150 }}
-              image="../src/images/pic9.jpg"
+              image={web}
               title="green iguana" />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
